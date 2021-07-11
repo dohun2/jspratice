6b46e08a4b270e6aef433fc3d9bd4e3f234e29eb
Dohun2 <dohun1221@icloud.com>
2021-07-11 17:19:24 +0900

    link delete

--- js/geetings.js
+++ js/geetings.js
@@ -2,10 +2,9 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting")
 
+
 const HIDDEN_CLASSNAME = "hidden"
 const USERNAME_KEY = "username"
-
-const link = document.querySelector("a")
 
 function onLoginSubmit(event) {
   event.preventDefault();
